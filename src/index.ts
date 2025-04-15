87f804800e7875f02482a49f693d2a51b23a0da5
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-04-15 18:19:43 +0530

    feat: graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,79 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-  res.send("TypeScript with nodejs");
-});
-app.listen(8000, () => console.log("server running at port 8000"));
+import express, { Request, Response } from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import bodyParser from "body-parser";
+import axios from "axios";
+import { gql } from "graphql-tag";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+async function startServer() {
+  const app = express();
+  app.use(bodyParser.json());
+
+  const typeDefs = gql`
+    type User {
+      id: ID!
+      name: String!
+      username: String!
+      email: String!
+      phone: String!
+      website: String!
+    }
+
+    type Todo {
+      id: ID!
+      title: String!
+      completed: Boolean
+    }
+
+    type Query {
+      getTodos: [Todo]
+      getAllUsers: [User]
+      getUser(id: ID!): User
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      getTodos: async (): Promise<Todo[]> => {
+        const res = await axios.get<Todo[]>(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        return res.data;
+      },
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(6050, () => {
+    console.log("Server running at http://localhost:6050/graphql");
+  });
+}
+
+startServer();
