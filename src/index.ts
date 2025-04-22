3571e309567c954a03d3c0fa44ae8d9652263a03
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-04-22 23:20:08 +0530

    chore: add express

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import { fetchUserProfile, fetchProblems } from "./services/userServices";
-
-const main = async () => {
+import express from "express";
+import { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT: number = Number(process.env.PORT);
+const app = express();
+app.get("/:username", async (req: Request, res: Response) => {
   try {
-    //const data = await fetchUserProfile("varshithsoma");
-    const problems = await fetchProblems("all-code-essentials", 20, 0, {});
-    console.log(problems.problemsetQuestionList.questions);
+    const username: string = req.params["username"];
+    const data = await fetchUserProfile(username);
+    console.log(data + " " + username);
+    res.send(data);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
-};
+});
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
+// const main = async () => {
+//   try {
+//     const data = await fetchUserProfile("");
+//     // const problems = await fetchProblems("all-code-essentials", 20, 0, {});
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-main();
+// main();
+// const name: string = "varshith";
