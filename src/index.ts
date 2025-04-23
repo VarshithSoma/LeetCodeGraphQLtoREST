69db05bee20a2bf057d7f05b78670447828fa981
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-04-24 00:37:32 +0530

    chore: add limit & problems endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,16 @@ app.get("/:username", async (req: Request, res: Response) => {
     console.log(error);
   }
 });
+app.get("/problems/:limit", async (req: Request, res: Response) => {
+  try {
+    const limit: number = Number(req.params["limit"]);
+    const data = await fetchProblems("all-code-essentials", limit, 0, {});
+    console.log(data + " " + limit);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
